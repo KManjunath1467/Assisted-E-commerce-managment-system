5156fce93dfa73e7f265b080d71fe63716cef711
K Manjunath <k2092003@gmail.com>
2026-09-19 17:37:02 +0530

    updated  theme-toggle component

--- frontend/components/theme-toggle.tsx
+++ frontend/components/theme-toggle.tsx
@@ -5,7 +5,7 @@ import { useTheme } from "next-themes";
 import { Button } from "@/components/ui/button";
 
 export function ThemeToggle() {
-  const { resolvedTheme, setTheme } = useTheme();
+  const { resolvedTheme, setTheme } = useTheme(); 
 
   return (
     <Button
